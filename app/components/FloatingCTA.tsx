fd28bf703fd5b9479a5cd3d3d76f9db532e840c7
sachinKumar8050 <sachinpal8050101@gmail.com>
2025-02-01 23:33:30 +0530

    update

--- app/components/FloatingCTA.tsx
+++ app/components/FloatingCTA.tsx
@@ -10,7 +10,7 @@ export default function FloatingCallButton() {
   };
   return (
     <motion.a
-      href="tel:+917500353702"
+      href="tel:7500353702"
       className="fixed bottom-8 right-8 z-50 bg-gradient-to-r from-[#EE8437] to-[#705541] text-white p-4 rounded-full shadow-lg hover:shadow-xl transition-all duration-300 flex items-center justify-center"
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
