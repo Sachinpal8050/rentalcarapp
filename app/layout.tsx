1aa2bc2a08439ba22af2e7396396bcc9111c68f3
sachinKumar8050 <sachinpal8050101@gmail.com>
2025-02-08 15:03:05 +0530

    quick fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "CarRental - Chauffeur-Driven Car Rental Service",
+  title: "MBT-Travels - Chauffeur-Driven Car Rental Service",
   description: "Affordable, reliable, and luxurious travel solutions with our chauffeur-driven car rental service.",
   icons: "/favicon.png",
 }
